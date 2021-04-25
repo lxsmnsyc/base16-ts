45dcdf7cac31b9f28fd35af7a32a54827db0b256
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-04-25 22:23:23 +0800

    Add Github link

--- home/components/Footer.tsx
+++ home/components/Footer.tsx
@@ -5,7 +5,12 @@ import Link from 'next/link';
 export default function Footer(): JSX.Element {
   return (
     <footer>
-      MIT © <Link href="https://github.com/lxsmnsyc">lxsmnsyc</Link>
+      <span>
+        MIT © <Link href="https://github.com/lxsmnsyc">lxsmnsyc</Link>
+      </span>
+      <span>
+        <Link href="https://github.com/lxsmnsyc/base16-ts">Github</Link>
+      </span>
     </footer>
   );
 }
